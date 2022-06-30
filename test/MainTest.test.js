1ffa1a7ec4da09e398c13278078b1ca28929bc61
Thorfy <lefebvre.remy.pro@gmail.Com>
2022-06-30 11:03:44 +0200

    prepare test for redeem

--- test/MainTest.test.js
+++ test/MainTest.test.js
@@ -34,6 +34,10 @@ contract("Init new Asset", (accounts) => {
       assert.equal(true, await bToken.isBToken());
       //add BToken from new Method
       await comptroller._supportMarket(bToken.address);
+
+      let allMarkets = await comptroller.getAllMarkets();
+      console.log(allMarkets);
+
     });
   });
 });
@@ -73,19 +77,19 @@ contract("Supply Asset", (accounts) => {
 
       await stableCoin.approve(bToken.address, 10000);
 
-      let mintResult = await bToken.mint(10);
+      await bToken.mint(10);
 
-      //load Btoken amount of stable
+      //load your amount of stable
       let accountBalanceOfStable = (
         await stableCoin.balanceOf(accounts[0])
       ).toNumber();
 
-      //load Btoken amount of stable
+      //load BToken amount of stable
       let bTokenBalanceOfStable = (
         await stableCoin.balanceOf(bToken.address)
       ).toNumber();
 
-      //load Btoken amount of stable
+      //load your amount of Btoken
       let accountBalanceOfBToken = (
         await bToken.balanceOf(accounts[0])
       ).toNumber();
@@ -93,54 +97,62 @@ contract("Supply Asset", (accounts) => {
       assert.equal(100000 - 10, accountBalanceOfStable);
       assert.equal(10, bTokenBalanceOfStable);
       assert.equal(10, accountBalanceOfBToken);
-
-      let allMarkets = await comptroller.getAllMarkets();
-      console.log(allMarkets);
     });
   });
-
-  // describe("Supply bMUsdt", async () => {
-  //   it("Mint should work", async () => {
-  //     await createBMusdtToken();
-  //     await stableCoin.approve(bMUsdt.address, 10000);
-  //     await tokenLending.mint("bMUsdt", 10);
-
-  //     let bMUsdtBalanceOfMUsdt = (
-  //       await stableCoin.balanceOf(bMUsdt.address)
-  //     ).toNumber();
-  //     let accountBalanceOfMUsdt = (
-  //       await stableCoin.balanceOf(accounts[0])
-  //     ).toNumber();
-  //     let accountBalanceOfBMUsdt = (
-  //       await bMUsdt.balanceOf(accounts[0])
-  //     ).toNumber();
-
-  //     assert.equal(10, bMUsdtBalanceOfMUsdt);
-  //     assert.equal(10, accountBalanceOfBMUsdt);
-
-  //     assert.equal(100000 - 10, accountBalanceOfMUsdt);
-  //   });
-
-  //   it("Redeem should work", async () => {
-  //     await createBMusdtToken();
-  //     await stableCoin.approve(bMUsdt.address, 10000);
-  //     // There is already 10 mint because of before test
-  //     await tokenLending.redeem("bMUsdt", 5);
-
-  //     let bMUsdtBalanceOfMUsdt = (
-  //       await stableCoin.balanceOf(bMUsdt.address)
-  //     ).toNumber();
-  //     let accountBalanceOfMUsdt = (
-  //       await stableCoin.balanceOf(accounts[0])
-  //     ).toNumber();
-  //     let accountBalanceOfBMUsdt = (
-  //       await bMUsdt.balanceOf(accounts[0])
-  //     ).toNumber();
-
-  //     assert.equal(5, bMUsdtBalanceOfMUsdt);
-  //     assert.equal(5, accountBalanceOfBMUsdt);
-
-  //     assert.equal(100000 - 5, accountBalanceOfMUsdt);
-  //   });
-  // });
 });
+
+contract("Redeem Asset", (accounts) => {
+
+  let comptroller, stableCoin, bToken;
+
+  before(async () => {
+    //contract load
+    comptroller = await Comptroller.new();
+    //create a stable Coin
+    stableCoin = await StableCoin.new("Mock USDT", "mUsdt", 18, 1000000000);
+    //transfer some Liquidity to admin
+    await stableCoin.adminTransfer(accounts[0], 100000);
+    //Create BToken
+    bToken = await BTokenImmutable.new(
+      stableCoin.address,
+      comptroller.address,
+      "bMUsdt",
+      "bMUsdt",
+      18,
+      accounts[0]
+    );
+    //Add BToken to Market Comptroller
+    await comptroller._supportMarket(bToken.address);
+    //approve transaction
+    await stableCoin.approve(bToken.address, 10);
+    //mint 10
+    await bToken.mint(10);
+  });
+
+  // Test redeem
+  describe("Swap Btoken To Get Stable", async () => {
+    it("you have btoken and you want stable", async () => {
+      assert.equal(true, await bToken.isBToken());
+      //approve transaction
+      await stableCoin.approve(bToken.address, 10);
+      //mint 10
+      await bToken.redeem(10);
+
+      //load your amount of stable
+      let accountBalanceOfStable = (
+        await stableCoin.balanceOf(accounts[0])
+      ).toNumber();
+      //load your amount of Btoken
+      let accountBalanceOfBToken = (
+        await bToken.balanceOf(accounts[0])
+      ).toNumber();
+
+      console.log("Stable in account[0]:",accountBalanceOfStable)
+      console.log("BToken in account[0]:",accountBalanceOfBToken)
+      assert.equal(100000, accountBalanceOfStable);
+      assert.equal(0, accountBalanceOfBToken);
+    });
+  });
+});
+
+
